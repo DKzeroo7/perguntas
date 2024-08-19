28c4522ab9e68e42646091f8bcf0b3145e9ee142
DKzeroo7 <114424441+DKzeroo7@users.noreply.github.com>
2024-08-19 12:00:31 -0300

    dvev

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const caixaResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-     enunciado: "A inteligência artificial (IA) é um conjunto de tecnologias que permitem aos computadores executar uma variedade de funções avançadas. Quais são as implicações da IA na privacidade dos dados?",
+     enunciado: "oi?",
        alternativas: [
            "A IA, embora possa desempenhar um papel fundamental na detecção e prevenção de ameaças à segurança.",
            "Também pode ser usada como uma ferramenta para a invasão de privacidade."
